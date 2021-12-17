8d9eb035ce01e0dae8e6713d6de4bb74a2e30529
LMaverick <91562063+LMaverick@users.noreply.github.com>
2021-12-17 16:05:47 -0300

    up

---
+++ Todos/systemRpg.js
@@ -0,0 +1,20 @@
+/*
+     força hulk= bombado
+     destreza john wick = precisão e agilidade
+     constituição superman = resistencia fisica
+
+     inteligencia sherlock holmes = conhecimentos especificos
+     sabedoria avó = conhecimentos gerais
+     carisma agostinho = lidar com pessoas
+
+     humano
+ */
+
+if (nome.lenght == 0){
+    var pri = getElementById
+}
+
+function pegarNome(){
+        var nome = document.getElementById('#nome')
+        alert('O nome é: ' + String(nome))
+    }
