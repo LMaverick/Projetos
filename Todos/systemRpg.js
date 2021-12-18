6760efafb315565a1696154a36d4e718d0b0cff0
LMaverick <91562063+LMaverick@users.noreply.github.com>
2021-12-18 13:37:49 -0300

    up

--- Todos/systemRpg.js
+++ Todos/systemRpg.js
@@ -13,6 +13,9 @@
  //==============================================================
 
 let person = ['', 'raca', 'classe']
+//     status: vigor, mana, força, destreza(velocidade e esquiva), constituição(resistencia fisica), inteligencia (conhecimentos especificos), sabedoria (conhecimentos gerais e experiencias), carisma (lidar com pessoas)
+//vigor, mana, força, destreza, consti, inteligencia, sabedoria, carisma
+let status = [100, 0, 0, 0, 0, 0, 0, 0]
 
 
 //==============================================================
@@ -36,3 +39,13 @@ function sumir(){
 }
 
 //==============================================================
+
+function pegarRaca(letra){
+    person[1] = document.getElementById('humano').onclick
+    alert(`sua raça é: ${person[1]}`)
+
+}
+var letra = function clickTeclado(letra) {
+    document.getElementById('qTeclado').onClick = letra;
+    alert(letra);
+    }
